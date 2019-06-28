a4a5faa16b0e9f81837f524f73e09aa68b2012f3
Ben Poon <Ben.Poon@microchip.com>
2019-06-28 10:35:58 +0800

    - wifi provisioning via wps application support is added for H3.

---
+++ app/wifi_provisioning_via_wps/firmware/src/config/sam_d21_xpro_winc1500/driver/winc/wdrv_winc_bssctx.c
@@ -0,0 +1,377 @@
+/*******************************************************************************
+  WINC Driver BSS Context Implementation
+
+  File Name:
+    wdrv_winc_bssctx.c
+
+  Summary:
+    WINC wireless driver BSS context implementation.
+
+  Description:
+    This interface manages the BSS contexts which 'wrap' the state
+      associated with BSSs.
+ *******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED AS IS WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+//DOM-IGNORE-END
+
+#include <stdint.h>
+#include <string.h>
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: File includes
+// *****************************************************************************
+// *****************************************************************************
+
+#include "wdrv_winc_common.h"
+#include "wdrv_winc_bssctx.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: WINC Driver BSS Context Implementation
+// *****************************************************************************
+// *****************************************************************************
+
+//*******************************************************************************
+/*
+  Function:
+    bool WDRV_WINC_BSSCtxIsValid
+    (
+        const WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+        bool ssidValid
+    )
+
+  Summary:
+    Tests if a BSS context is valid.
+
+  Description:
+    Tests the elements of the BSS context to judge if their values are legal.
+
+  Precondition:
+    None.
+
+  Parameters:
+    pBSSCtx   - Pointer to a BSS context.
+    ssidValid - Flag indicating if the SSID within the context must be valid.
+
+  Returns:
+    true or false indicating if context is valid.
+
+  Remarks:
+    See wdrv_winc_bssctx.h for usage information.
+
+*/
+
+bool WDRV_WINC_BSSCtxIsValid
+(
+    const WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+    bool ssidValid
+)
+{
+    /* Ensure BSS context is valid. */
+    if (NULL == pBSSCtx)
+    {
+        return false;
+    }
+
+    /* Ensure the channels are valid. */
+    if ((!pBSSCtx->channel) ||
+        ((pBSSCtx->channel > 14) && (pBSSCtx->channel != WDRV_WINC_ALL_CHANNELS)))
+    {
+        return false;
+    }
+
+    /* Ensure the SSID length is valid. */
+    if (pBSSCtx->ssid.length > 32)
+    {
+        return false;
+    }
+
+#ifdef WDRV_WINC_DEVICE_EXT_CONNECT_PARAMS
+    /* Ensure the BSSID is non-zero, if valid. */
+    if (true == pBSSCtx->bssid.valid)
+    {
+        int i;
+        uint8_t macAddrChk;
+
+        macAddrChk = 0;
+        for (i=0; i<M2M_MAC_ADDRES_LEN; i++)
+        {
+            macAddrChk |= pBSSCtx->bssid.addr[i];
+        }
+
+        if (0 == macAddrChk)
+        {
+            return false;
+        }
+    }
+#endif    
+
+    if (true == ssidValid)
+    {
+        /* If requested, check the SSID is present. */
+        if (0 == pBSSCtx->ssid.length)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//*******************************************************************************
+/*
+  Function:
+    WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetDefaults
+    (
+        WDRV_WINC_BSS_CONTEXT *const pBSSCtx
+    )
+
+  Summary:
+    Configures a BSS context into a default legal state.
+
+  Description:
+    Ensures that each element of the structure is configured into a legal state.
+
+  Remarks:
+    See wdrv_winc_bssctx.h for usage information.
+
+*/
+
+WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetDefaults
+(
+    WDRV_WINC_BSS_CONTEXT *const pBSSCtx
+)
+{
+    /* Ensure BSS context is valid. */
+    if (NULL == pBSSCtx)
+    {
+        return WDRV_WINC_STATUS_INVALID_ARG;
+    }
+
+#ifdef WDRV_WINC_DEVICE_EXT_CONNECT_PARAMS
+    memset(pBSSCtx->bssid.addr, 0, M2M_MAC_ADDRES_LEN);
+    pBSSCtx->bssid.valid = false;
+#endif    
+
+    /* Set context to have no SSID, all channels and not cloaked. */
+    pBSSCtx->ssid.length = 0;
+    pBSSCtx->channel     = WDRV_WINC_ALL_CHANNELS;
+    pBSSCtx->cloaked     = false;
+
+    return WDRV_WINC_STATUS_OK;
+}
+
+//*******************************************************************************
+/*
+  Function:
+    WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetSSID
+    (
+        WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+        uint8_t *const pSSID,
+        uint8_t ssidLength
+    )
+
+  Summary:
+    Configures the SSID of the BSS context.
+
+  Description:
+    The SSID string and length provided the SSID is copied into the BSS
+      context.
+
+  Remarks:
+    See wdrv_winc_bssctx.h for usage information.
+
+*/
+
+WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetSSID
+(
+    WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+    uint8_t *const pSSID,
+    uint8_t ssidLength
+)
+{
+    /* Ensure BSS context and SSID buffer and length are valid. */
+    if ((NULL == pBSSCtx) || (NULL == pSSID) || (ssidLength > 32))
+    {
+        return WDRV_WINC_STATUS_INVALID_ARG;
+    }
+
+    /* Copy the SSID ensure unused space is zeroed. */
+    memset(&pBSSCtx->ssid.name, 0, 32);
+    memcpy(&pBSSCtx->ssid.name, pSSID, ssidLength);
+    pBSSCtx->ssid.length = ssidLength;
+
+    /* Validate context. */
+    if (false == WDRV_WINC_BSSCtxIsValid(pBSSCtx, false))
+    {
+        return WDRV_WINC_STATUS_INVALID_CONTEXT;
+    }
+
+    return WDRV_WINC_STATUS_OK;
+}
+
+//*******************************************************************************
+/*
+  Function:
+    WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetBSSID
+    (
+        WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+        uint8_t *const pBSSID
+    )
+
+  Summary:
+    Configures the BSSID of the BSS context.
+
+  Description:
+    The BSSID string is copied into the BSS context.
+
+  Remarks:
+    See wdrv_winc_bssctx.h for usage information.
+
+*/
+
+#ifdef WDRV_WINC_DEVICE_EXT_CONNECT_PARAMS
+WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetBSSID
+(
+    WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+    uint8_t *const pBSSID
+)
+{
+    /* Ensure BSS context and SSID buffer and length are valid. */
+    if ((NULL == pBSSCtx) || (NULL == pBSSID))
+    {
+        return WDRV_WINC_STATUS_INVALID_ARG;
+    }
+
+    if (NULL != pBSSID)
+    {
+        /* Copy the BSSID. */
+        memcpy(&pBSSCtx->bssid, pBSSID, M2M_MAC_ADDRES_LEN);
+        pBSSCtx->bssid.valid = true;
+    }
+    else
+    {
+        memset(&pBSSCtx->bssid, 0, M2M_MAC_ADDRES_LEN);
+        pBSSCtx->bssid.valid = false;
+    }
+
+    /* Validate context. */
+    if (false == WDRV_WINC_BSSCtxIsValid(pBSSCtx, false))
+    {
+        return WDRV_WINC_STATUS_INVALID_CONTEXT;
+    }
+
+    return WDRV_WINC_STATUS_OK;
+}
+#endif
+
+//*******************************************************************************
+/*
+  Function:
+    WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetChannel
+    (
+        WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+        uint8_t channel
+    )
+
+  Summary:
+    Configures the channel of the BSS context.
+
+  Description:
+    The supplied channel value is copied into the BSS context.
+
+  Remarks:
+    See wdrv_winc_bssctx.h for usage information.
+
+*/
+
+WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetChannel(
+                        WDRV_WINC_BSS_CONTEXT *const pBSSCtx, uint8_t channel)
+{
+    /* Ensure BSS context and channels are valid. */
+    if ((NULL == pBSSCtx) || (!channel) ||
+        ((channel > 14) && (channel != WDRV_WINC_ALL_CHANNELS)))
+    {
+        return WDRV_WINC_STATUS_INVALID_ARG;
+    }
+
+    /* Copy channel. */
+    pBSSCtx->channel = channel;
+
+    /* Validate context. */
+    if (false == WDRV_WINC_BSSCtxIsValid(pBSSCtx, false))
+    {
+        return WDRV_WINC_STATUS_INVALID_CONTEXT;
+    }
+
+    return WDRV_WINC_STATUS_OK;
+}
+
+//*******************************************************************************
+/*
+  Function:
+    WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetSSIDVisibility
+    (
+        WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+        bool visible
+    )
+
+  Summary:
+    Configures the visibility of the BSS context.
+
+  Description:
+    Specific to Soft-AP mode this flag defines if the BSS context will create a
+      visible presence on air.
+
+  Remarks:
+    See wdrv_winc_bssctx.h for usage information.
+
+*/
+
+WDRV_WINC_STATUS WDRV_WINC_BSSCtxSetSSIDVisibility
+(
+    WDRV_WINC_BSS_CONTEXT *const pBSSCtx,
+    bool visible
+)
+{
+    /* Ensure BSS context is valid. */
+    if (NULL == pBSSCtx)
+    {
+        return WDRV_WINC_STATUS_INVALID_ARG;
+    }
+
+    /* Set cloaked state. */
+    pBSSCtx->cloaked = visible ? false : true;
+
+    /* Validate context. */
+    if (false == WDRV_WINC_BSSCtxIsValid(pBSSCtx, false))
+    {
+        return WDRV_WINC_STATUS_INVALID_CONTEXT;
+    }
+
+    return WDRV_WINC_STATUS_OK;
+}
---
+++ app/wifi_provisioning_via_wps/firmware/src/example.c
@@ -0,0 +1,322 @@
+/*******************************************************************************
+  WINC Example Application
+
+  File Name:
+    wifi_provisioning_via_wps.c
+
+  Summary:
+    Wi-Fi Provisioning via WPS Example.
+
+  Description:
+    This example demonstrates the use of the WINC1500 with the SAMD21 Xplained Pro
+    board to start Wi-Fi provisioning mode via WPS
+
+    The configuration defines for this demo are:
+        MAIN_WPS_PUSH_BUTTON_FEATURE           -- Select to test WPS push button mode or WPS pin mode
+        MAIN_WPS_PIN_NUMBER        -- Set the WPS pin number 
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+
+/** \mainpage
+ * \section intro Introduction
+ * This example demonstrates the use of the WINC1500 with the SAMD21 Xplained Pro board
+ * to connect to AP with WPS Security.<br>
+ * It uses the following hardware:
+ * - the SAMD21 Xplained Pro.
+ * - the WINC1500 on EXT1.
+ *
+ *
+ * \section usage Usage
+ * -# Assemble the devices and connect to USB debug cable from PC.
+ * -# On the computer, open and configure a terminal application as the follows.
+ * \code
+ *    Baud Rate : 115200
+ *    Data : 8bit
+ *    Parity bit : none
+ *    Stop bit : 1bit
+ *    Flow control : none
+ * \endcode
+ *
+ * -# Prepare an AP that supports Wi-Fi Protected Setup(WPS)
+ *
+ * -# To test WPS button method, Use case 1 in example.c function.
+ * -# Configure below code in the example_conf.h for WPS push button feature.
+ * \code
+ *    #define MAIN_WPS_PUSH_BUTTON_FEATURE              "true"
+ * \endcode
+ * -# Press SW0  button at SAMD21 XPRO
+ * -# Press WPS button at the AP. ( For more information, please refer to AP product documentation )
+ * -# Run the application. The WINC1500 will be connected to the AP automatically without security information.
+ * \code
+ * ===================================
+ * Wi-Fi Provisioning via WPS Example
+ * ===================================
+ * SW0 button pressed
+ * Device is connecting using WPS Push Button option
+ * [APP_ExampleWPSPushButtonDiscoveryCallback] In
+ * SSID : xxxxx, authtyp : x pw : xxxxxxxx
+ * Connect AP, SSID = xxxxx
+ * STA mode: Station connected
+ * STA Mode: Station IP address is xxx.xxx.xxx.xxx
+
+ * \endcode
+ *
+ * -# To test WPS PIN method, Use case 2 in example.c function.
+ * -# Configure below code in the main.h for WPS PIN number and WPS push button feature.
+ * \code
+ *    #define WPS_PIN_NUMBER                            "12345670"
+ *    #define MAIN_WPS_PUSH_BUTTON_FEATURE              "false"
+ * \endcode
+ * -# Enter WPS PIN number in the AP setup menu and start the AP. ( For more information, please refer to AP product documentation )
+ * -# Run the application. The WINC1500 will be connected to the AP automatically without security information.
+ * \code
+ * ===================================
+ * Wi-Fi Provisioning via WPS Example
+ * ===================================
+ * [APP_ExampleWPSPinDiscoveryCallback] In
+ * SSID : xxxxx, authtyp : x pw : xxxxxxxx
+ * Connect AP, SSID = xxxxx
+ * STA mode: Station connected
+ * STA Mode: Station IP address is xxx.xxx.xxx.xxx
+ * \endcode
+ */
+ 
+#include "app.h"
+#include "wdrv_winc_client_api.h"
+#include "example_conf.h"
+
+typedef enum
+{
+    /* Example's state machine's initial state. */
+    EXAMP_STATE_INIT=0,
+    EXAMP_STATE_WPS_PIN,
+    EXAMP_STATE_WPS_PUSH_BUTTON, 
+    EXAMP_STATE_WPS_PUSH_BUTTON_WAITING,
+    EXAMP_STATE_WPS_CHECK_BUTTON,
+    EXAMP_STATE_CONNECT_AP,    
+    EXAMP_STATE_CONNECTING_AP,
+    EXAMP_STATE_CONNECTED_AP,
+    EXAMP_STATE_DISCONNECTED,
+    EXAMP_STATE_ERROR,
+} EXAMP_STATES;
+
+static EXAMP_STATES state;
+static WDRV_WINC_BSS_CONTEXT  bssCtx;
+static WDRV_WINC_AUTH_CONTEXT authCtx;
+static bool gbPressButton = false;
+
+
+
+/**
+ * \brief button pressed callback function.
+ */
+static void btn_press(uint8_t btn, uint8_t press)
+{
+	if (MAIN_SW1 == btn) {
+            if (press ==  0) {
+                APP_DebugPrintf("SW0 button pressed\r\n");
+                state = EXAMP_STATE_WPS_PUSH_BUTTON;
+            }
+	}
+}
+
+/**
+ * \brief Initialize buttons driver.
+ */
+static void btn_init(void)
+{
+    if (GPIO_PA15_Get() == 0)
+    {
+        btn_press(MAIN_SW1, 0);
+        gbPressButton = true;
+    }
+}
+
+static void APP_ExampleWPSPinDiscoveryCallback(DRV_HANDLE handle, WDRV_WINC_BSS_CONTEXT *pBSSCtx, WDRV_WINC_AUTH_CONTEXT *pAuthCtx)
+{
+   APP_DebugPrintf("[APP_ExampleWPSPinDiscoveryCallback] In\r\n"); 
+   APP_DebugPrintf("SSID : %s, authtyp : %d pw : %s\r\n", pBSSCtx->ssid.name, pAuthCtx->authType, pAuthCtx->authInfo.PSK);
+    if (pAuthCtx == NULL) {
+        APP_DebugPrintf("WPS is not enabled OR Timedout\r\n");
+        return;
+    }    
+   memcpy(&bssCtx, pBSSCtx, sizeof(WDRV_WINC_BSS_CONTEXT));
+   memcpy(&authCtx, pAuthCtx, sizeof(WDRV_WINC_AUTH_CONTEXT));
+   state = EXAMP_STATE_CONNECT_AP;
+}
+
+static void APP_ExampleWPSPushButtonDiscoveryCallback(DRV_HANDLE handle, WDRV_WINC_BSS_CONTEXT *pBSSCtx, WDRV_WINC_AUTH_CONTEXT *pAuthCtx)
+{
+    APP_DebugPrintf("[APP_ExampleWPSPushButtonDiscoveryCallback] In\r\n");
+    APP_DebugPrintf("SSID : %s, authtyp : %d pw : %s\r\n", pBSSCtx->ssid.name, pAuthCtx->authType, pAuthCtx->authInfo.PSK);
+    if (pAuthCtx == NULL) {
+        APP_DebugPrintf("WPS is not enabled OR Timedout\r\n");
+        state = EXAMP_STATE_WPS_CHECK_BUTTON;
+        gbPressButton = false;
+        return;
+    }    
+    memcpy(&bssCtx, pBSSCtx, sizeof(WDRV_WINC_BSS_CONTEXT));
+    memcpy(&authCtx, pAuthCtx, sizeof(WDRV_WINC_AUTH_CONTEXT));
+    state = EXAMP_STATE_CONNECT_AP;
+
+}
+
+
+static void APP_ExampleSTAConnectNotifyCallback(DRV_HANDLE handle, WDRV_WINC_CONN_STATE currentState, WDRV_WINC_CONN_ERROR errorCode)
+{
+    if (WDRV_WINC_CONN_STATE_CONNECTED == currentState)
+    {
+        APP_DebugPrintf("STA mode: Station connected\r\n");
+        state = EXAMP_STATE_CONNECTED_AP;
+        
+      
+        
+        
+    }
+    else if (WDRV_WINC_CONN_STATE_DISCONNECTED == currentState)
+    {
+        APP_DebugPrintf("STA mode: Station disconnected\r\n");
+
+
+        state = EXAMP_STATE_DISCONNECTED;
+    }
+}
+
+
+static void APP_ExampleSTAModeDHCPAddressEventCallback(DRV_HANDLE handle, uint32_t ipAddress)
+{
+    char s[20];
+
+    APP_DebugPrintf("STA Mode: Station IP address is %s\r\n", inet_ntop(AF_INET, &ipAddress, s, sizeof(s)));
+}
+
+void APP_ExampleInitialize(DRV_HANDLE handle)
+{
+    APP_DebugPrintf("\r\n");
+    APP_DebugPrintf("===================================\r\n");
+    APP_DebugPrintf("Wi-Fi Provisioning via WPS Example\r\n");
+    APP_DebugPrintf("===================================\r\n");
+
+    state = EXAMP_STATE_INIT;
+
+}
+
+void APP_ExampleTasks(DRV_HANDLE handle)
+{
+
+    switch (state)
+    {
+        case EXAMP_STATE_INIT:
+        {
+           
+            /* Preset the error state incase any following operations fail. */
+
+            state = EXAMP_STATE_ERROR; 
+            
+            char devName[] = "WINC1500_WPS";
+            
+            
+            if (WDRV_WINC_STATUS_OK != WDRV_WINC_InfoDeviceNameSet(handle, devName))
+            {
+                break;
+            }
+
+
+            if (!MAIN_WPS_PUSH_BUTTON_FEATURE) {
+                
+                /* case 2 WPS PIN method */
+                if (WDRV_WINC_STATUS_OK != WDRV_WINC_WPSEnrolleeDiscoveryStartPIN(handle, MAIN_WPS_PIN_NUMBER, &APP_ExampleWPSPinDiscoveryCallback))
+                {
+                    break;
+                }
+                
+                state = EXAMP_STATE_WPS_PIN;
+            }
+            
+            /* case 1 WPS Push Button method. */
+            state = EXAMP_STATE_WPS_CHECK_BUTTON;
+            break;
+        }
+
+        case EXAMP_STATE_WPS_PIN:
+        {
+           
+            break;
+        }
+
+        case EXAMP_STATE_WPS_CHECK_BUTTON:
+        {
+            if (!gbPressButton) 
+            {
+                btn_init();
+            }
+            break;
+        }
+        
+        case EXAMP_STATE_WPS_PUSH_BUTTON:
+        {
+            if (WDRV_WINC_STATUS_OK != WDRV_WINC_WPSEnrolleeDiscoveryStartPBC(handle, APP_ExampleWPSPushButtonDiscoveryCallback))
+            {
+                break;
+            }
+            
+            APP_DebugPrintf("Device is connecting using WPS Push Button option\r\n");
+            state = EXAMP_STATE_WPS_PUSH_BUTTON_WAITING;
+            
+            break;
+        }
+
+        
+        case EXAMP_STATE_CONNECT_AP:
+        {
+            if (WDRV_WINC_STATUS_OK != WDRV_WINC_IPUseDHCPSet(handle, &APP_ExampleSTAModeDHCPAddressEventCallback))
+            {
+                APP_DebugPrintf("WDRV_WINC_IPUseDHCPSet() fail ...\r\n");
+                break;
+            }
+            
+            APP_DebugPrintf("Connect AP, SSID = %s\r\n",bssCtx.ssid.name);
+            if (WDRV_WINC_STATUS_OK != WDRV_WINC_BSSConnect(handle, &bssCtx, &authCtx, APP_ExampleSTAConnectNotifyCallback))
+            {
+                break;
+            }
+            state = EXAMP_STATE_CONNECTING_AP;
+            break;
+        }
+
+        case EXAMP_STATE_ERROR:
+        {
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+}
+
+// DOM-IGNORE-END
