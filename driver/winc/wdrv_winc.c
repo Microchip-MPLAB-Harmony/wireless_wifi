22335a5382af07f8fc705ff9be81d41b9d64d50b
Ben Poon <Ben.Poon@microchip.com>
2020-06-19 15:25:15 +0800

    [MH3-36974]:correct the handling for the mutex creation

--- driver/winc/wdrv_winc.c
+++ driver/winc/wdrv_winc.c
@@ -1151,9 +1151,11 @@ void WDRV_WINC_Tasks(SYS_MODULE_OBJ object)
 
             WDRV_DBG_INFORM_PRINT("WINC: Initializing...\r\n");
             
-            if(OSAL_RESULT_TRUE != OSAL_MUTEX_Create(&pDcpt->eventProcessMutex))
+            if (OSAL_RESULT_TRUE != OSAL_MUTEX_Create(&pDcpt->eventProcessMutex))
             {
+                WDRV_DBG_ERROR_PRINT("eventProcessMutex create failed\r\n");
                 pDcpt->sysStat = SYS_STATUS_ERROR;
+                break;
             }
 
             /* Initialise SPI handling. */
